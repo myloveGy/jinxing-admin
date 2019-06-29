59f2d8918a2a5eaf13261ed73851d659a046e4a0
刘金星 <821901008@qq.com>
2019-06-29 09:50:18 +0800

    refactor: 资源路径问题

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp')
 var minify = require('gulp-minify')
+var cssMinify = require('gulp-minify-css')
 
 gulp.task('default', function () {
   // 将你的默认的任务代码放在这
@@ -10,6 +11,14 @@ gulp.task('default', function () {
       },
     }))
     .pipe(gulp.dest('./dist/js/common'))
+
+  gulp.src('./src/css/!(*.min.css)')
+    .pipe(cssMinify({
+      ext: {
+        min: '.min.css',
+      },
+    }))
+    .pipe(gulp.dest('./dist/css'))
 })
 
 
